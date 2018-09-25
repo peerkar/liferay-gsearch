09d67d3d0c3b96cd79543a82fd5f9ff61bc2c055
Jussi Nevalainen <jussi.nevalainen@ambientia.fi>
2018-09-25 15:36:11 +0300

    Disabled unused resultLayoutOptions URL parameter

--- gsearch-web/src/main/resources/META-INF/resources/View.es.js
+++ gsearch-web/src/main/resources/META-INF/resources/View.es.js
@@ -22,38 +22,38 @@ import templates from './View.soy';
 class View extends Component {
 
 	constructor(opt_config) {
-		
-		super(opt_config);		
-		
+
+		super(opt_config);
+
 		this.debug = opt_config.JSDebugEnabled;
 
-		this.initialQueryParameters = opt_config.initialQueryParameters; 
+		this.initialQueryParameters = opt_config.initialQueryParameters;
 
 		this.searchResultsURL = opt_config.searchResultsURL;
 
 		this.portletNamespace = opt_config.portletNamespace;
-		
+
 		this.queryMinLength = opt_config.queryMinLength;
 
 		this.requestTimeout = opt_config.requestTimeout;
 
-		// If this was linked call i.e. if keyword parameter was present in the calling url, 
+		// If this was linked call i.e. if keyword parameter was present in the calling url,
 		// then execute search. Notice that nested templates are processed before parent.
-		
+
 		let q = this.initialQueryParameters['q'];
 
 		if (q && q.length > 0) {
 
 			this.query.isInitialQuery = true;
 			this.executeQuery();
-			
+
 		} else {
 
 			// Reset possibly cached state
-			
+
 			this.query.oldParameters = [];
 			this.query.parameters = [];
-			
+
 		}
 	}
 
@@ -61,7 +61,7 @@ class View extends Component {
 	 * @inheritDoc
 	 */
 	attached() {
-		
+
 		if (this.debug) {
 			console.log("View.attached()");
 		}
@@ -74,33 +74,33 @@ class View extends Component {
 	addResultsCallback(func) {
 		this.resultsCallbacks.push(func);
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	created() {
-		
+
 		if (this.debug) {
 			console.log("View.created()");
 		}
 
-		// Create query object. 
-		// Need to create the query object here for that to be available 
+		// Create query object.
+		// Need to create the query object here for that to be available
 		// for the nested templates. Constructor of this template
 		// will be run after child templates.
-			
+
 		this.query = new GSearchQuery();
 	}
-		
+
 	/**
 	 * Get query param
-	 * 
+	 *
 	 * @param {String} key
 	 * @param {Boolean} singleValue
-	 * 
+	 *
 	 */
 	getQueryParam(key, singleValue) {
-		
+
 		if (singleValue) {
 			return this.query.getParameterValue(key);
 		} else {
@@ -110,12 +110,12 @@ class View extends Component {
 
 	/**
 	 * Set query param
-	 * 
+	 *
 	 * @param {String} key
 	 * @param {String} value
 	 * @param {Boolean} refresh
-	 * @param {Boolean} isMultiValued 
-	 * @param {String} previousValue 
+	 * @param {Boolean} isMultiValued
+	 * @param {String} previousValue
 	 */
 	setQueryParam(key, value, refresh=true, isMultiValued=true, previousValue=null) {
 
@@ -127,42 +127,42 @@ class View extends Component {
 
 			this.query.setMultiValueParameter(key, value, previousValue);
 		}
-				
+
 		if (refresh) {
 			this.executeQuery();
 		}
 	}
-	
+
 	/**
 	 * Execute query.
 	 */
 	executeQuery() {
-		
+
 		if (this.debug) {
 			console.log("Executing query.");
 		}
 
-		// Check that there's a query 
-		
+		// Check that there's a query
+
 		let q = this.query.getParameterValue('q');
-		
-		if (!q || q.length < this.queryMinLength) {		
-			
+
+		if (!q || q.length < this.queryMinLength) {
+
 			return;
 		}
-		
+
 		// Hide elements and show loader image.
 
 		this.setLoading(true);
 
 		// Clean filter parameters on subsequent queries
-		
+
 		this.query.cleanFilterParameters();
-		
+
 		// Build params.
-		
+
 		let params = this.query.buildQueryParams();
-				
+
 		Ajax.request(
 				this.searchResultsURL,
 				'GET',
@@ -173,39 +173,39 @@ class View extends Component {
 		).then((response) => {
 
 			if (response.responseText) {
-				
+
 				// Set results object.
-				
+
 				this.results =  JSON.parse(response.responseText);
-														
+
 				// Remove loading placeholder.
 
 				this.setLoading(false);
-				
+
 				// Update browser address bar.
 
 				this.updateAddressBar(this.query.buildAddressBarURL());
-				
+
 				// Set result layout options
-				
-				this.setResultLayoutOptions();
+
+			//	this.setResultLayoutOptions();
 
 				// Run callbacks
-				
+
 				let length = this.resultsCallbacks.length;
-				
+
 				for (let i = 0; i < length; i++) {
 
 					let f = this.resultsCallbacks[i];
-									
+
 					f(this.portletNamespace, this.results);
 				}
-				
+
 			} else {
-				
+
 				// Assume here simply that there was an error if response was empty.
 				// Make better by sending proper response codes from backend etc.
-				
+
 				alert(Liferay.Language.get('there-was-an-error'));
 			}
 		}).catch(function(reason) {
@@ -213,7 +213,7 @@ class View extends Component {
 			alert(Liferay.Language.get('there-was-an-error'));
 		});
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
@@ -225,13 +225,13 @@ class View extends Component {
 
 	/**
 	 * Show/hide ajax loader image.
-	 * 
+	 *
 	 * @param {boolean} isLoading
 	 */
 	setLoading(isLoading) {
 
 		let resultElement = $('#' + this.portletNamespace + 'SearchResults');
-		
+
 		if (isLoading) {
 			resultElement.html('');
 			resultElement.addClass('ajax-loader-placeholder');
@@ -239,32 +239,32 @@ class View extends Component {
 			resultElement.removeClass('ajax-loader-placeholder');
 		}
 	}
-	
+
 	/**
 	 * @inheritDoc
 	 */
 	setResultLayoutOptions() {
-		
+
 		// Show image layout option if type filter is "file" or extension is "image".
-		
+
 		if (this.getQueryParam('type', true) == 'file' || this.getQueryParam('extension', true) == 'Image') {
-			
+
 			$('#' + this.portletNamespace + 'LayoutOptions .image-layout').removeClass('hide');
 		} else {
 
 			$('#' + this.portletNamespace + 'LayoutOptions .image-layout').addClass('hide');
 		}
-		
+
 		// We might have a forced layout from results
-		
+
 		if (this.results) {
 			this.setQueryParam('resultsLayout', this.results.meta.resultsLayout, false, false);
 		}
 	}
-	
+
 	/**
 	 * Update address bar.
-	 * 
+	 *
 	 * @param {address} key
 	 */
 	updateAddressBar(address) {
@@ -272,13 +272,13 @@ class View extends Component {
 			window.history.pushState(null, this.query.getParameterValue('q') + '-' + Liferay.Language.get('search'), address);
 		} else {
 			document.location.hash = address;
-		}		
+		}
 	}
 }
 
-/** 
+/**
  * State definition.
- * 
+ *
  * @type {!Object}
  * @static
  */
@@ -287,7 +287,7 @@ View.STATE = {
 		value: null
 	},
 	resultsCallbacks: {
-		value: [] 
+		value: []
 	}
 };
 
