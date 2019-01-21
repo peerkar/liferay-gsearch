e583cdddfc9ddcf49c2c65d2ec2ffe7ed48c363f
Petteri Karttunen <petteri.karttunen@liferay.com>
2019-01-21 12:26:59 +0200

    More Like This for 7.0

---
+++ gsearch-more-like-this/src/main/java/fi/soveltia/liferay/gsearch/morelikethis/constants/GSearchMoreLikeThisPortletKeys.java
@@ -0,0 +1,12 @@
+package fi.soveltia.liferay.gsearch.morelikethis.constants;
+
+/**
+ * Portlet names.
+ * 
+ * @author Petteri Karttunen
+ */
+public class GSearchMoreLikeThisPortletKeys {
+
+	public static final String MORE_LIKE_THIS_PORTLET = "morelikethisportlet";
+
+}
