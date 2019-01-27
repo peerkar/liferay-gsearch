8a261617a0f677f028f7aeaa4a9629f9a967d726
Petteri Karttunen <petteri.karttunen@liferay.com>
2019-01-27 11:05:25 +0200

    Additional Fields initial commit.

---
+++ gsearch-additional-document-fields/README.md
@@ -0,0 +1,18 @@
+# Liferay Additional Document Fields
+
+Adds following fields into index:
+
+* gsearch_version_count: number of versions
+
+Using function score query these fields can be used to boost relevant results.
+
+## Prerequisites
+
+The Liferay GSearch custom Elasticsearch adapter.
+
+## Configuration
+
+After the module has been deployed succesfully, please see the configuration options in Control Panel -> Configuration -> System Settings -> Liferay GSearch -> Additional Index Fields
+
+This feature is disabled by default. 
+
---
+++ gsearch-additional-document-fields/src/main/java/fi/soveltia/liferay/gsearch/additionalfields/configuration/ModuleConfiguration.java
@@ -0,0 +1,38 @@
+package fi.soveltia.liferay.gsearch.additionalfields.configuration;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * Additional document fields configuration.
+ * 
+ * @author Petteri Karttunen
+ *
+ */
+@ExtendedObjectClassDefinition(
+	category = "GSearch"
+)
+@Meta.OCD(
+	id = "fi.soveltia.liferay.gsearch.additionalfields.configuration.ModuleConfiguration",
+	localization = "content/Language",
+	name = "additional-fields-configuration"
+)
+public interface ModuleConfiguration {
+
+	@Meta.AD(
+		deflt = "false", 
+	    description = "enable-feature-desc",
+	    name = "enable-feature-name",
+		required = false
+	)
+	public boolean enableFeature();
+	
+	@Meta.AD(
+		deflt = "gsearch_version_count", 
+	    description = "version-field-desc",
+	    name = "version-field-name",
+		required = false
+	)
+	public String versionField();
+}
