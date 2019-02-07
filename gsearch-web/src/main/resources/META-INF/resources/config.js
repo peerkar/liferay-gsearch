28434567a2fade1627bd73a7c55b84b192b72493
Petteri Karttunen <petteri.karttunen@liferay.com>
2019-02-07 18:13:28 +0200

    Loader exports.

--- gsearch-web/src/main/resources/META-INF/resources/config.js
+++ gsearch-web/src/main/resources/META-INF/resources/config.js
@@ -1,21 +1,19 @@
-Liferay.Loader.define('jquery', function(){
+Liferay.Loader.define('jquery', function() {
 	return window.jQuery;
 });
 
-Liferay.Loader.addModule(
-  {
-    name: 'google-maps-loader',
-    dependencies: [],
-    anonymous: true,
-    path: MODULE_PATH + '/js/Google.js',
-    exports: 'GoogleMapsLoader'
-  }
-);
-
 Liferay.Loader.addModule({
-  name        : 'bootstrap-datepicker',
-  dependencies: ['jquery'],
-  anonymous   : true,
-  path        : MODULE_PATH + '/js/bootstrap-datepicker/js/bootstrap-datepicker.js'
+	name : 'google-maps-loader',
+	dependencies : [],
+	anonymous : true,
+	path : MODULE_PATH + '/js/Google.js',
+	exports : 'GoogleMapsLoader'
 });
 
+Liferay.Loader.addModule({
+	name : 'bootstrap-datepicker',
+	anonymous : true,
+	dependencies : [ 'jquery' ],
+	exports : '$.fn.datepicker',
+	path : MODULE_PATH + '/js/bootstrap-datepicker/js/bootstrap-datepicker.js'
+});
