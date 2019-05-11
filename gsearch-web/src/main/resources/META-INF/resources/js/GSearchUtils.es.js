6af825f01ed75f25d502578c7f4dadb2856ad563
Petteri Karttunen <petteri.karttunen@liferay.com>
2019-05-12 01:07:04 +0300

    Time filter fixes.

--- gsearch-web/src/main/resources/META-INF/resources/js/GSearchUtils.es.js
+++ gsearch-web/src/main/resources/META-INF/resources/js/GSearchUtils.es.js
@@ -218,7 +218,7 @@ class GSearchUtils {
 				GSearchUtils.unsetOptionListSelectedItem(component, optionMenu, paramName, isMultiValued, value);
 			}
 
-			GSearchUtils.setOptionListTriggerElementText(optionMenu, null, paramName);
+			GSearchUtils.setOptionListTriggerElementText(optionMenu, selectedItems, paramName);
 
 			event.preventDefault();
 		});			
