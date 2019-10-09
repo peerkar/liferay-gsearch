0a4a9900d34f054f5df8504c57c58f24ee6faf19
Jussi Nevalainen <jussi.nevalainen@ambientia.fi>
2019-10-09 16:48:48 +0300

    Added javax.portlet.version 3.0 to portlets, fixed com.linkedin.urls in bnd

--- gsearch-more-like-this/src/main/java/fi/soveltia/liferay/gsearch/morelikethis/portlet/GSearchMoreLikeThisPortlet.java
+++ gsearch-more-like-this/src/main/java/fi/soveltia/liferay/gsearch/morelikethis/portlet/GSearchMoreLikeThisPortlet.java
@@ -26,7 +26,8 @@ import fi.soveltia.liferay.gsearch.morelikethis.constants.GSearchMoreLikeThisPor
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + GSearchMoreLikeThisPortletKeys.MORE_LIKE_THIS_PORTLET,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
